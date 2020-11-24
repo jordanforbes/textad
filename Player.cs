096e25448a4d50d25f249d222d8c25a721ba95e2
jordanforbes <jordforb@gmail.com>
2020-11-24 12:54:25 -0500

    initial commit

---
+++ Player.cs
@@ -0,0 +1,12 @@
+namespace textad
+{
+    public class Player
+    {
+        public string name;
+        public int coins = 0;
+        public int health = 10;
+        public int damage = 1;
+        public int armorValue = 0;
+        public int potion = 5;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace textad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Start();
+        }
+
+        static void Start()
+        {
+            Console.WriteLine("Minotaur's Dungeon");
+            Console.WriteLine("Name: ");
+            Console.ReadLine();
+        }
+    }
+}
